0c5cc48320fcdc4d6c025e955647af3d5bf6110a
Glen Maddern <glenmaddern@gmail.com>
2013-07-25 09:31:25 +1000

    whoa!

--- canvasgif.js
+++ canvasgif.js
@@ -16,49 +16,42 @@
     window.frames = frames;
     sgCanvas.remove();
 
-    var camera, scene, renderer;
-    var geometry, material;
-    var textures;
+    var camera, controls, scene, renderer;
 
     function init() {
         camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 1, 10000 );
         camera.position.z = 600;
 
-        window.camera = camera;
+        controls = new THREE.TrackballControls( camera );
 
         scene = new THREE.Scene();
 
-        geometry = new THREE.CubeGeometry( width, height, width );
-
-      textures = [];
       for (var i = 0, l = frames.length; i < l; i++) {
+        var geometry = new THREE.CubeGeometry( width, height, 0 );
+
         var texture = new THREE.Texture(frames[i].data);
         texture.needsUpdate = true;
-        textures.push(texture);
+
+        var material = new THREE.MeshBasicMaterial( { map: texture} );
+
+        var mesh = new THREE.Mesh( geometry, material );
+        mesh.position.set(0, 0, -50 * i);
+
+        scene.add( mesh );
       }
 
-        material = new THREE.MeshBasicMaterial( { map: textures[0]} );
-        window.material = material;
+      renderer = new THREE.WebGLRenderer();
+      renderer.setSize( window.innerWidth, window.innerHeight );
 
-        window.mesh = new THREE.Mesh( geometry, material );
-        scene.add( window.mesh );
-
-        renderer = new THREE.WebGLRenderer();
-        renderer.setSize( window.innerWidth, window.innerHeight );
-
-        document.body.appendChild( renderer.domElement );
+      document.body.appendChild( renderer.domElement );
 
     }
 
     function animate() {
-      currentFrame = (currentFrame + 1) % frames.length;
       // note: three.js includes requestAnimationFrame shim
       requestAnimationFrame( animate );
 
-      material.map = textures[currentFrame];
-
-      mesh.rotation.x += 0.001;
-      mesh.rotation.y += 0.02;
+      controls.update();
 
       renderer.render( scene, camera );
     }
